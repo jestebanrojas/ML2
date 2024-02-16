4ad6b3c26aa1439b73d38b3aed813f138427de1b
jestebanrojas <149639844+jestebanrojas@users.noreply.github.com>
2024-02-15 19:40:34 -0500

    Delete foto directory

--- foto/foto.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image, ImageOps 
-
-image = Image.open(r"foto/juan_Rojas.jpg") 
-
-new_image = image.resize((256, 256))
-new_image = ImageOps.grayscale(new_image) 
-new_image.save('foto/Juan_Rojas1.jpg')
-
